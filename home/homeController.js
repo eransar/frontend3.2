dc47189dc3947f7889a282fb1811af5e26b732f7
erans <erans@post.bgu.ac.il>
2019-06-15 10:39:26 +0300

    added ngcookies support in app and index.html fixed search controller error login now working with redirection

--- home/homeController.js
+++ home/homeController.js
@@ -1,17 +1,65 @@
-angular.module("myApp")
-    .controller("homeController", function ($scope) {
-        
-//         $scope.submit = function(){
-//         var email = function(){
+angular.module("myApp").controller("homeController", function ($scope,$http,$location,$cookies) {
 
-//         }
 
-//         var user = $scope.username;
-//         if(/^[a-zA-Z]+$/.test(user)){
-//                 console.log(user);
-//         }
-//         else{
+        $scope.loginClick = function(){
+            var username1=document.getElementById("user_box").value;
+            var password1=document.getElementById("passwd_box").value;
 
-//         }
-//     }
-    });
+
+            if(username1==="" || password1===""){
+                return;
+            }
+            logindata = {
+                username : username1,
+                password : password1
+            };
+            var usertoken;
+            try{
+                 usertoken = $cookies.get('token');
+            }
+            catch (e) {
+
+            }
+
+
+            if(usertoken !=null){
+                $http({
+                    method : "POST",
+                    url : "http://localhost:3000/login",
+                    data : logindata,
+                    headers: {
+                        'Authorization': usertoken
+                    },
+                }).then(function mySuccess(response) {
+
+                    $scope.token = response.data;
+                    $location.path('/login');
+                }, function myError(response) {
+                    $scope.loginerror = response.data;
+                    alert($scope.loginerror);
+                });
+            }
+            else {
+                $http({
+                    method : "POST",
+                    url : "http://localhost:3000/login",
+                    data : logindata
+                }).then(function mySuccess(response) {
+
+                    $scope.token = response.data;
+                    $cookies.put('token',$scope.token);
+                    $location.path('/login');
+                }, function myError(response) {
+                    // console.log(response.statusText);
+                    $scope.loginerror = response.data;
+                    alert($scope.loginerror);
+
+
+                });
+            }
+
+
+        };
+
+});
+
